591d33b00a6081004e7fedb7b3a3d4468494c134
songrun <songrunmo@gmail.com>
2026-01-02 15:22:45 +0800

    cli args parse

---
+++ src/Cli.zig
@@ -0,0 +1,72 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const Writer = std.Io.Writer;
+const eql = std.mem.eql;
+
+const Self = @This();
+
+exe_dir: []const u8,
+mode: Mode,
+version: []const u8,
+
+pub const Mode = enum { zig, zls };
+
+pub const help =
+    \\Update zig & zls
+    \\
+    \\Usage: ztup <mode>
+    \\
+    \\Modes:
+    \\  zig <version>   Update zig
+    \\  zls <version>   Update zls
+    \\  -h --help       Print help
+    \\
+;
+
+pub const Error = error{
+    PrintHelp,
+    FieldMissing,
+    UnknownMode,
+};
+
+pub fn parse(allocator: Allocator, writer: *Writer) !Self {
+    var iter = try std.process.argsWithAllocator(allocator);
+    defer iter.deinit();
+
+    const exe_path = iter.next().?;
+    const exe_dir = std.fs.path.dirname(exe_path).?;
+    const mode_str = iter.next() orelse {
+        std.log.err("Missing field 'mode'", .{});
+        return Error.FieldMissing;
+    };
+    const mode = mode: {
+        if (eql(u8, mode_str, "-h") or eql(u8, mode_str, "--help")) {
+            try writer.print("{s}\n", .{help});
+            try writer.flush();
+            return Error.PrintHelp;
+        } else if (eql(u8, mode_str, "zig")) {
+            break :mode Mode.zig;
+        } else if (eql(u8, mode_str, "zls")) {
+            break :mode Mode.zls;
+        } else {
+            std.log.err("Unknown mode '{s}'", .{mode_str});
+            return Error.UnknownMode;
+        }
+    };
+    const version = iter.next() orelse {
+        std.log.err("Missing field 'version'", .{});
+        return Error.FieldMissing;
+    };
+
+    return .{
+        .exe_dir = try allocator.dupe(u8, exe_dir),
+        .mode = mode,
+        .version = try allocator.dupe(u8, version),
+    };
+}
+
+pub fn deinit(self: *Self, allocator: Allocator) void {
+    allocator.free(self.exe_dir);
+    allocator.free(self.version);
+    self.* = undefined;
+}
--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,22 @@
 const std = @import("std");
 
-pub fn main() !void {}
+const Cli = @import("Cli.zig");
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.smp_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    var threaded = std.Io.Threaded.init(allocator, .{});
+    defer threaded.deinit();
+    const io = threaded.io();
+
+    const stdout = std.Io.File.stdout();
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = stdout.writer(io, &stdout_buffer);
+
+    var cli = Cli.parse(allocator, &stdout_writer.interface) catch |err| {
+        if (err == Cli.Error.PrintHelp) return else return err;
+    };
+    defer cli.deinit(allocator);
+}
